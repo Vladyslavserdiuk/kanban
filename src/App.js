150f3275dc76fc1c4f3498df66ad8fbc6108b309
Vladyslav <Vladyslav.serdiuk@gmail.com>
2020-08-16 19:23:10 +0200

    install uuid

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import React, {useState} from 'react';
 import FormCreateTask from "./FormCreateTask";
 import Bord from "./Bord";
 
+import {v4 as uuidv4} from 'uuid';
+
 
 function App() {
 
     const initialTask = [
         {
-            id: 1,
+            id: uuidv4(),
             title: 'First Task',
             priority: 2,
             status: 'todo'
         },
         {
-            id: 2,
+            id: uuidv4(),
             title: 'Second Task',
             priority: 3,
             status: 'review'
-        },
-
-
-    ]
+        }
+    ];
 
     const [tasks, setTasks] = useState()
 
